75017a879b02e4f5d77a51d8adc59e74be183407
Isaac Attuah <isaacattuah@gmail.com>
2020-02-18 08:59:11 -0500

    Add failsafe for elements beyond 16

--- SortLib.js
+++ SortLib.js
@@ -15,7 +15,7 @@
 //The following functions input the sorted array into the console
 
 
-    
+
 
 
 //This function adds inputted numbers to arrays
@@ -29,63 +29,73 @@ function addTo() {
 
 //Insertion Sort Algorithm Implementation
 insertionSort = (array) => {
-  
-    for (outer = 1; outer < array.length; outer++) {
-        for (inner = 0; inner < outer; inner++) {
-            console.log(array.join(' '))
-            if (array[outer] < array[inner]) {
-                const [element] = array.splice(outer, 1)
-                array.splice(inner, 0, element)
+
+    if (array.length >= 16) {
+        console.log("You have more than 16 elements. Cannot solve this problem")
+    } else {
+        for (outer = 1; outer < array.length; outer++) {
+            for (inner = 0; inner < outer; inner++) {
+                console.log(array.join(' '))
+                if (array[outer] < array[inner]) {
+                    const [element] = array.splice(outer, 1)
+                    array.splice(inner, 0, element)
+                }
             }
         }
+        console.log(array.join(' '))
+        document.getElementById("sort").innerHTML = "You array has been sorted using Insertion Sort"
+        return array
     }
-    console.log(array.join(' '))
-    document.getElementById("sort").innerHTML = "You array has been sorted using Insertion Sort"
-    return array
 }
 
 //Bubble Sort Algorithm Implementation
 bubbleSort = (array) => {
-    
-    let swapped = false
-    do {
-        swapped = false
-        array.forEach((current, i) => {
-            console.log(array.join(' '))
-            if (current > array[i + 1]) {
-                const temp = current
+    if (array.length >= 16) {
+        console.log("You have more than 16 elements. Cannot solve this problem")
+    } else {
+        let swapped = false
+        do {
+            swapped = false
+            array.forEach((current, i) => {
                 console.log(array.join(' '))
+                if (current > array[i + 1]) {
+                    const temp = current
+                    console.log(array.join(' '))
 
-                array[i] = array[i + 1]
-                array[i + 1] = temp
-                swapped = true
-            }
-        })
-    } while (swapped)
-    console.log(array.join(' '))
-    document.getElementById("sort").innerHTML = "You array has been sorted using Bubble Sort"
-    return array
+                    array[i] = array[i + 1]
+                    array[i + 1] = temp
+                    swapped = true
+                }
+            })
+        } while (swapped)
+        console.log(array.join(' '))
+        document.getElementById("sort").innerHTML = "You array has been sorted using Bubble Sort"
+        return array
+    }
 }
 
 //Bubble Sort Algorithm Implementation
 quickSort = (array) => {
-    
-    if (array.length < 2) {
-        return array
-    }
-    const chosenIndex = array.length - 1
-    const chosen = array[chosenIndex]
-    const a = []
-    const b = []
-    for (let i = 0; i < chosenIndex; i++) {
-        const temp = array[i]
-        temp < chosen ? a.push(temp) : b.push(temp)
-    }
+    if (array.length >= 16) {
+        console.log("You have more than 16 elements. Cannot solve this problem")
+    } else {
+        if (array.length < 2) {
+            return array
+        }
+        const chosenIndex = array.length - 1
+        const chosen = array[chosenIndex]
+        const a = []
+        const b = []
+        for (let i = 0; i < chosenIndex; i++) {
+            const temp = array[i]
+            temp < chosen ? a.push(temp) : b.push(temp)
+        }
 
-    const output = [...quickSort(a), chosen, ...quickSort(b)]
-    console.log(output.join(' '))
-    document.getElementById("sort").innerHTML = "You array has been sorted using Quick Sort"
-    return output
+        const output = [...quickSort(a), chosen, ...quickSort(b)]
+        console.log(output.join(' '))
+        document.getElementById("sort").innerHTML = "You array has been sorted using Quick Sort"
+        return output
+    }
 }
 
 
@@ -95,11 +105,11 @@ divide = (array) => {
     if (array.length < 2) {
         return array
     }
-        const mid = Math.floor(array.length / 2)
-        const smallOne = array.slice(0, mid)
-        const smallTwo = array.slice(mid)
-        return sort(divide(smallOne), divide(smallTwo))
-    }
+    const mid = Math.floor(array.length / 2)
+    const smallOne = array.slice(0, mid)
+    const smallTwo = array.slice(mid)
+    return sort(divide(smallOne), divide(smallTwo))
+}
 
 sort = (smallOne, smallTwo) => {
     const sorted = []
@@ -108,22 +118,21 @@ sort = (smallOne, smallTwo) => {
             sorted.push(smallOne.shift())
         } else {
             sorted.push(smallTwo.shift())
-            }
         }
-    
+    }
+
     const output = [...sorted, ...smallOne, ...smallTwo]
     console.log(output)
-    
+
     return output
-    }
+}
 
 let emptyArray = []
 mergeSort = (array) => {
-document.getElementById("sort").innerHTML = "You array has been sorted using Merge Sort"
-return sort(divide(array), emptyArray)
+    if (array.length >= 16) {
+        console.log("You have more than 16 elements. Cannot solve this problem")
+    } else {
+        document.getElementById("sort").innerHTML = "You array has been sorted using Merge Sort"
+        return sort(divide(array), emptyArray)
+    }
 }
-
-
-
-  
-    
